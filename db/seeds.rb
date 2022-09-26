681f0f97e357c1ba2b6cef13fdf13d74c6579a2a
mengrui song <summerrock1017@gmail.com>
2022-09-26 18:21:33 +0900

    changed the link to be white

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,22 +6,22 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-require 'open-uri'
-Movie.destroy_all
-# List.destroy_all
+# require 'open-uri'
+# Movie.destroy_all
+# # List.destroy_all
 
-# the Le Wagon copy of the API
-url = 'http://tmdb.lewagon.com/movie/top_rated'
-response = JSON.parse(URI.open(url).read)
+# # the Le Wagon copy of the API
+# url = 'http://tmdb.lewagon.com/movie/top_rated'
+# response = JSON.parse(URI.open(url).read)
 
-response['results'].each do |movie_hash|
-  # create an instance with the hash
-  Movie.create!(
-    title: movie_hash['title'],
-    overview: movie_hash['overview'],
-    poster_url: 'https://image.tmdb.org/t/p/w500' + movie_hash['poster_path'],
-    rating: movie_hash['vote_average']
-  )
-end
+# response['results'].each do |movie_hash|
+#   # create an instance with the hash
+#   Movie.create!(
+#     title: movie_hash['title'],
+#     overview: movie_hash['overview'],
+#     poster_url: 'https://image.tmdb.org/t/p/w500' + movie_hash['poster_path'],
+#     rating: movie_hash['vote_average']
+#   )
+# end
 
-puts "#{Movie.count} movies have been created!"
+# puts "#{Movie.count} movies have been created!"
