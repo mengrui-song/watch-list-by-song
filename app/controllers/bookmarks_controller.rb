b6fe651ed097c2da4114a32352222e53b4e410ab
mengrui song <summerrock1017@gmail.com>
2022-09-26 15:30:56 +0900

    used tmdb to search movie

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,3 +1,6 @@
+require 'open-uri'
+require 'json'
+
 class BookmarksController < ApplicationController
   def new
     @list = List.find(params[:list_id])
@@ -7,7 +10,14 @@ class BookmarksController < ApplicationController
 
   def create
     @list = List.find(params[:list_id])
-    @bookmark = Bookmark.new(bookmark_params)
+		if Movie.find_by(title: params[:bookmark][:movie])
+			@movie = Movie.find_by(title: params[:bookmark][:movie])
+		else
+			@movie = fetch_movie(params[:bookmark][:movie])
+      @movie.save
+		end
+    @comment = params[:bookmark][:comment]
+    @bookmark = Bookmark.new(movie: @movie, comment: @comment)
     @bookmark.list = @list
     if @bookmark.save
       redirect_to list_path(@list)
@@ -27,4 +37,17 @@ class BookmarksController < ApplicationController
   def bookmark_params
     params.require(:bookmark).permit(:comment, :movie_id)
   end
+
+	def fetch_movie(movie_name)
+    url = "https://api.themoviedb.org/3/search/movie?api_key=b04a4d29fa7cbdec4d7960abf964d46f&language=en-US&query=#{movie_name}&page=1&include_adult=false"
+    response = JSON.parse(URI.open(url).read)
+    movie_hash = response['results'][0]
+    @movie = Movie.new(
+      title: movie_hash['title'],
+      overview: movie_hash['overview'],
+      poster_url: "https://image.tmdb.org/t/p/w500#{movie_hash['poster_path']}",
+      rating: movie_hash['vote_average']
+    )
+	end
+
 end
