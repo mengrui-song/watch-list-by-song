5fd45c921f247495755b693b7a155fc478c6bf98
mengrui song <summerrock1017@gmail.com>
2022-11-28 23:51:06 +0900

    changed style

--- app/javascript/controllers/typed_js_controller.js
+++ app/javascript/controllers/typed_js_controller.js
@@ -4,8 +4,8 @@ import Typed from "typed.js"
 export default class extends Controller {
   connect() {
     new Typed(this.element, {
-      strings: ["Keep calm", "Make a watch list."],
-      typeSpeed: 50,
+      strings: ["I believe if there's any kind of God it wouldn't be in any of us, not you or me but just this little space in between.", "If there's any kind of magic in this world it must be in the attempt of understanding someone sharing something."],
+      typeSpeed: 20,
       loop: true
     })
   }
