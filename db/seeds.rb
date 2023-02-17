3c3e8e2bcceda8e67e040a49b2ef2af62160a424
song <song@IH-PPC109R.local>
2023-02-17 10:34:57 +0900

    added seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,30 +5,43 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-
-# require 'open-uri'
-# Movie.destroy_all
-# # List.destroy_all
-
-# # the Le Wagon copy of the API
-# url = 'http://tmdb.lewagon.com/movie/top_rated'
-# response = JSON.parse(URI.open(url).read)
-
-# response['results'].each do |movie_hash|
-#   # create an instance with the hash
-#   Movie.create!(
-#     title: movie_hash['title'],
-#     overview: movie_hash['overview'],
-#     poster_url: 'https://image.tmdb.org/t/p/w500' + movie_hash['poster_path'],
-#     rating: movie_hash['vote_average']
-#   )
-# end
-
-
-# puts "#{Movie.count} movies have been created!"
+Bookmark.destroy_all
+Movie.destroy_all
+List.destroy_all
 User.destroy_all
-User.create!(
+
+require 'open-uri'
+# List.destroy_all
+
+# the Le Wagon copy of the API
+url = 'http://tmdb.lewagon.com/movie/top_rated'
+response = JSON.parse(URI.open(url).read)
+
+response['results'].each do |movie_hash|
+  # create an instance with the hash
+  Movie.create!(
+    title: movie_hash['title'],
+    overview: movie_hash['overview'],
+    poster_url: 'https://image.tmdb.org/t/p/w500' + movie_hash['poster_path'],
+    rating: movie_hash['vote_average']
+  )
+end
+
+
+puts "#{Movie.count} movies have been created!"
+user = User.create!(
   email: "summerrock1017@gmail.com",
   password: "123123"
 )
 puts "Create a user."
+
+mylist = List.create!(
+  name: "2023",
+  image_url: "https://picsum.photos/200/300",
+  user: user
+)
+puts "A list created!"
+Movie.all.each do |movie|
+  Bookmark.create!(movie: movie, list: mylist)
+end
+puts "added movies to my list."
